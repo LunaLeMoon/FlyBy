22aff927868d2ad506732c955406d22f3689dc61
Le Moon Studio <97724060+LunaLeMoon@users.noreply.github.com>
2023-06-02 04:58:10 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'LeMoon Studio'
+description 'A script that would make a plane fly across the map.'
+version 'v0.4.0-beta'
+
+
+server_script 'flyby_server.lua'
